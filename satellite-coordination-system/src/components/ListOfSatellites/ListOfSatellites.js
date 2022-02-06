b258c8b17ebcdd71024f6a21283ac7dfe209fa23
sarakim1309 <victoriasarakim.sk@gmail.com>
2022-02-06 17:50:25 -0300

    search by name done

--- satellite-coordination-system/src/components/ListOfSatellites/ListOfSatellites.js
+++ satellite-coordination-system/src/components/ListOfSatellites/ListOfSatellites.js
@@ -9,43 +9,71 @@ export default function ListOfSattellites() {
   const [value, setValue] = useState(false);
 
 	useEffect(()=>{
+    const listener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
 		fetch('https://api.spacexdata.com/v5/launches/')
 			.then(response => response.json())
 			.then(setList);
 		}, []);
 
+  const filterNames = (names, query) => {
+    if (!query) {
+        return names;
+    }
+
+    return names.filter((item) => {
+        const postItem = item.name.toLowerCase();
+        return postItem.includes(query) || item.name.includes(query);
+    });
+  };
+
   const { search } = window.location;
   const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const filteredNames = filterNames(list, searchQuery);
 
 	return (
 		<div>
-      <SearchName/> 
+      <SearchName
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
       <table className="table_fixed_header">
     		<thead>
     			<tr>
     				<th>Name</th>
     				<th>UTC Date</th>
-            <th>Succesfull</th>
+            <th>Succesful</th>
     			</tr>
     		</thead>
     		<tbody>
-    			{list.sort((a, b) => a.name.localeCompare(b.name)).map((item) => ((value && item.success &&
-                      <tr key={item.name}>
-                        <td>{item.name}</td>
-                        <td>{item.date_utc}</td>
-                        <td>{(item.success && "Yes") || "No"}</td>
-                      </tr>) || (!value &&
-                      <tr key={item.name}>
-                        <td>{item.name}</td>
-                        <td>{item.date_utc}</td>
-                        <td>{(item.success && "Yes") || "No"}</td>
-                      </tr>)
+    			{filteredNames.sort((a, b) => a.name.localeCompare(b.name))
+            .map((item) => (
+              (value && item.success &&
+              <tr key={item.name}>
+                <td>{item.name}</td>
+                <td>{item.date_utc}</td>
+                <td>{(item.success && "Yes") || "No"}</td>
+              </tr>) || 
+              (!value &&
+              <tr key={item.name}>
+                <td>{item.name}</td>
+                <td>{item.date_utc}</td>
+                <td>{(item.success && "Yes") || "No"}</td>
+              </tr>)
     			))}
     		</tbody>
     	</table>
-      <div className="container">
+      <div className="container"> 
         {Switch(value, () => setValue(!value))} Show only successful launches
-      </div>
+      </div> 
     </div>
 	)
 }
--- satellite-coordination-system/src/components/SearchName/SearchName.js
+++ satellite-coordination-system/src/components/SearchName/SearchName.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./SearchName.css";
 
 
-const SearchBar = () => (
+const SearchBar = ({ searchQuery, setSearchQuery }) => (
     <form action="/" method="get">
         <label htmlFor="header-search">
             <span className="visually-hidden"></span>
         </label>
         <input
+            value={searchQuery}
+            onInput={e => setSearchQuery(e.target.value)}
+            onKeyDown={e => setSearchQuery(e.target.value)}
             className="searcher"
             type="text"
-            id="header-search"
+            // id="header-search"
             placeholder="Search a satellite name"
-            name="s" 
+            // name="s" 
         />
-        <button type="submit" className="button" >Search</button>
     </form>
 );
 
